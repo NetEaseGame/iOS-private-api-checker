cc92dbe223d7368498847e59cf26616f33f1eca6
hustcc <i@hust.cc>
2015-11-18 17:19:05 +0800

    fix codesign command ouput

--- dump/codesign_utils.py
+++ dump/codesign_utils.py
@@ -18,8 +18,6 @@ def codesignapp(app_path):
     Returns:
         the content of codesign
     """
-
-    codesign_cmd = "codesign -dvv %s" # codesign cmd模板字符串
-    cmd = codesign_cmd % app_path
-    out = subprocess.check_output(cmd.split())
+    cmd = "/usr/bin/codesign -dvvv %s" % app_path
+    out = commands.getstatusoutput(cmd)
     return out
